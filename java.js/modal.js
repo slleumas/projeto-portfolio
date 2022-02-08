9569d15f471524ab4ea4d45dfdbddd4590adf928
slleumas <agenciasuca@gmail.com>
2022-02-08 17:28:09 -0300

    cria galeria de imagens

---
+++ java.js/modal.js
@@ -0,0 +1,133 @@
+function iniciaModal(modalID){
+    const modal= document.getElementById(modalID);
+    if(modal){
+        modal.classList.add('show-modal');
+        modal.addEventListener('click', (e)=>{
+            if (e.target.id == modalID || e.target.className == 'exit'){
+                modal.classList.remove('show-modal')
+                
+            }
+        })
+    }
+}
+
+const foto= document.querySelector('.foto01');
+foto.addEventListener('click',function(){
+ iniciaModal('imgjs01')  
+ })
+   const foto2= document.querySelector('.foto02');
+   foto2.addEventListener('click',function(){
+   iniciaModal('imgjs02')  
+ })
+ const foto3= document.querySelector('.foto03');
+   foto3.addEventListener('click',function(){
+   iniciaModal('imgjs03')  
+ })
+const foto4= document.querySelector('.foto04');
+   foto4.addEventListener('click',function(){
+   iniciaModal('imgjs04')  
+ })
+ const foto5= document.querySelector('.foto05');
+   foto5.addEventListener('click',function(){
+   iniciaModal('imgjs05')  
+ })
+ const foto6= document.querySelector('.foto06');
+   foto6.addEventListener('click',function(){
+   iniciaModal('imgjs06')  
+ })
+ const foto7= document.querySelector('.foto07');
+   foto7.addEventListener('click',function(){
+   iniciaModal('imgjs07')  
+ })
+ const foto8= document.querySelector('.foto08');
+   foto8.addEventListener('click',function(){
+   iniciaModal('imgjs08')  
+ })
+ const foto9= document.querySelector('.foto09');
+   foto9.addEventListener('click',function(){
+   iniciaModal('imgjs09')  
+ })
+ const foto10= document.querySelector('.foto10');
+ foto10.addEventListener('click',function(){
+ iniciaModal('imgjs10')  
+})
+const foto11= document.querySelector('.foto11');
+foto11.addEventListener('click',function(){
+iniciaModal('imgjs11')  
+})
+const foto12= document.querySelector('.foto12');
+foto12.addEventListener('click',function(){
+iniciaModal('imgjs12')  
+})
+const foto13= document.querySelector('.foto13');
+foto13.addEventListener('click',function(){
+iniciaModal('imgjs13')  
+})
+const foto14= document.querySelector('.foto14');
+foto14.addEventListener('click',function(){
+iniciaModal('imgjs14')  
+})
+const foto15= document.querySelector('.foto15');
+foto15.addEventListener('click',function(){
+iniciaModal('imgjs15')  
+})
+const foto16= document.querySelector('.foto16');
+foto16.addEventListener('click',function(){
+iniciaModal('imgjs16')  
+})
+const foto17= document.querySelector('.foto17');
+foto17.addEventListener('click',function(){
+iniciaModal('imgjs17')  
+})
+const foto18= document.querySelector('.foto18');
+foto18.addEventListener('click',function(){
+iniciaModal('imgjs18')  
+})
+const foto19= document.querySelector('.foto19');
+foto19.addEventListener('click',function(){
+iniciaModal('imgjs19')  
+})
+const foto20= document.querySelector('.foto20');
+foto20.addEventListener('click',function(){
+iniciaModal('imgjs20')  
+})
+const foto21= document.querySelector('.foto21');
+foto21.addEventListener('click',function(){
+iniciaModal('imgjs21')  
+})
+const foto22= document.querySelector('.foto22');
+foto22.addEventListener('click',function(){
+iniciaModal('imgjs22')  
+})
+const foto23= document.querySelector('.foto23');
+foto23.addEventListener('click',function(){
+iniciaModal('imgjs23')  
+})
+const foto24= document.querySelector('.foto24');
+foto24.addEventListener('click',function(){
+iniciaModal('imgjs24')  
+})
+const foto25= document.querySelector('.foto25');
+foto25.addEventListener('click',function(){
+iniciaModal('imgjs25')  
+})
+const foto26= document.querySelector('.foto26');
+foto26.addEventListener('click',function(){
+iniciaModal('imgjs26')  
+})
+const foto27= document.querySelector('.foto27');
+foto27.addEventListener('click',function(){
+iniciaModal('imgjs27')  
+})
+const foto28= document.querySelector('.foto28');
+foto28.addEventListener('click',function(){
+iniciaModal('imgjs28')  
+})
+const foto29= document.querySelector('.foto29');
+foto29.addEventListener('click',function(){
+iniciaModal('imgjs29')  
+})
+const foto30= document.querySelector('.foto30');
+foto30.addEventListener('click',function(){
+iniciaModal('imgjs30')  
+})
